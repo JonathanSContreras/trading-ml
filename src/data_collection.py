2f46a55358f7b0eb04f8c9af3ab8b55fdd81480a
Jonathan Contreras <jonathansaulcontreras@gmail.com>
2026-02-09 22:06:50 -0600

    updating code to get 2 years worth of data, not just one

--- src/data_collection.py
+++ src/data_collection.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 tickers = ["SPY", "QQQ", "AAPL", "MSFT", "TSLA"]
 start_date = "2024-01-01"
-end_date = "2025-02-09"
+end_date = "2026-02-09"
 
 for ticker in tickers:
     print(f"Downloading data for {ticker}...")
